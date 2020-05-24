6be9a0c30455bdfef1acf81cf77e1990a50fa8ab
Eddie Kim <eddienakkim@gmail.com>
2020-05-23 22:02:08 -0700

    set up firebase connection and tested

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,34 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAUuKAKICdIn5KfiE4IPggcmf6josM__gw",
-    authDomain: "class-project-bab91.firebaseapp.com",
-    databaseURL: "https://class-project-bab91.firebaseio.com",
-    projectId: "class-project-bab91",
-    storageBucket: "class-project-bab91.appspot.com",
-    messagingSenderId: "1031038654969",
-    appId: "1:1031038654969:web:a20d1760da945a23054528",
-    measurementId: "G-LE4B97CM2C"
+var config = {
+    apiKey: "AIzaSyCEgOutiR_BTJMCdip9xHceEeqD41WImy8",
+    authDomain: "class-project-f8b83.firebaseapp.com",
+    databaseURL: "https://class-project-f8b83.firebaseio.com/",
+    projectId: "class-project-f8b83",
+    storageBucket: "class-project-f8b83.appspot.com",
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
-  var trainData = firebase.database();
+  var database = firebase.database();
 
-  $("#submitbtn").on("click", function(){
-      var trainName = $("#name-input").val().trim()
-      var dest = $("#destination-input").val().trim()
-      var firstTrain = $("#firstTrain-input").val().trim()
-      var freq = $("#frequency-input").val().trim()
+  $("#submitbtn").on("click", function(event){
+    event.preventDefault();
+    var trainName = $("#name-input").val().trim()
+    var dest = $("#destination-input").val().trim()
+    var firstTrain = moment($("#firstTrain-input").val().trim(), "HH:MM").subtract(10, "years").format("X");
+    var freq = $("#frequency-input").val().trim()
+
+    var newTrain = {
+        name: trainName,
+        destination: dest,
+        firstTrain: firstTrain,
+        frequency: freq
+    }
+
+    database.ref().push(newTrain)
+
+    $("#name-input").val("")
+    $("#destination-input").val("")
+    $("#firstTrain-input").val("")
+    $("#frequency-input").val("")
+
   })
